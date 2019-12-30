a1ce8860dd0fefee763cd6cabe31709bc69c7634
wachukxs <nwachukwuossai@gmail.com>
2019-12-30 03:33:53 +0100

    hmmm

--- src/app/dialogs/dialogs.component.ts
+++ src/app/dialogs/dialogs.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit, Inject } from '@angular/core';
-// import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {MatDatepickerInputEvent} from '@angular/material/datepicker';
-
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 // import {MatExpansionModule, MatExpansionPanelHeader, MatExpansionPanelDescription} from '@angular/material';
 
 export interface DialogData {
+  code: string;
   animal: 'panda' | 'unicorn' | 'lion';
   arr: Array<any>;
   dep: Array<any>;
 }
 
 
-/* @NgModule({
+@NgModule({
   imports: [
+    HttpClient
   ],
-}) */
+})
 
 
 @Component({
@@ -26,14 +28,51 @@ export interface DialogData {
 })
 export class DialogsComponent implements OnInit {
   panelOpenState = false;
-  selected: string;
+  selected = 'departure';
   time: string;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+  timeUnix: string;
+  all: Array<any>;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private http: HttpClient) { }
 
   addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
     // this.events.push(`${type}: ${event.value}`);
-    console.log(`${type}: ${event.value.toDateString()}`);
+    console.log(`${type}: ${event.value.toDateString()} | for ${this.data.code}`);
+
+    console.log(`${event.value.toISOString()}:it`);
+
     this.time = event.value.toDateString();
+    this.timeUnix = event.value.toUTCString();
+    if (this.selected !== undefined) {
+
+      // The given time interval must not be larger than 30 days!
+
+      // https://electrictoolbox.com/unix-timestamp-javascript/
+      // https://flaviocopes.com/how-to-get-timestamp-javascript/
+
+      // https://stackoverflow.com/a/15911310
+      const later = Math.floor(new Date(event.value.toISOString()).getTime() / 1000) + (7 * 24 * 60 * 60);
+      const g = `https://opensky-network.org/api/flights/${this.selected}/?airport=${this.data.code}&begin=${Math.floor(new Date(event.value.toISOString()).getTime() / 1000).toString()}&end=${later}`;
+      
+      console.log(`make call ${g}`);
+
+      this.http.get(g, {
+        headers: new HttpHeaders({
+          'Access-Control-Allow-Origin': '*/*'
+        })
+      }).subscribe((data: Array<object>) => {
+        
+        if (data.length > 20) {
+          // truncate it and alert user
+          data.length = 20;
+          this.all = data;
+          
+        }
+        console.log('fetched data:', data);
+        // draw the button when we get here later
+      }, // success path
+        error => { console.error('we had err fetching:', error); } // error path
+      );
+    }
   }
 
   ngOnInit() {
